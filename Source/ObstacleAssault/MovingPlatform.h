c791838750228aed8ffaa89e75db01c5aaf68e35
Alexandre Nascimento <alexandre.nascimento.ifms@gmail.com>
2024-02-21 18:46:03 -0400

    feat: rotação dos obstaculos implementada

--- Source/ObstacleAssault/MovingPlatform.h
+++ Source/ObstacleAssault/MovingPlatform.h
@@ -23,18 +23,23 @@ private:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	UPROPERTY(EditAnywhere, Category = "Moving Platform")
+	UPROPERTY(EditAnywhere, Category = "Moving")
 	FVector PlatformVelocity = FVector(100, 0, 0);
 
-	UPROPERTY(EditAnywhere, Category = "Moving Platform")
+	UPROPERTY(EditAnywhere, Category = "Moving")
 	float MoveDistance = 100;
+
+	UPROPERTY(EditAnywhere, Category = "Moving")
+	bool PauseBeforeLoop = false;
+
+	UPROPERTY(EditAnywhere, Category = "Rotation")
+	FRotator RotationVelocity;
 
 	FVector StartLocation;
 
 	void MovePlatform(float DeltaTime);
 	void RotatePlatform(float DeltaTime);
-	
-	bool ShouldPlatformReturn();
 
-	float GetDistanceMoved();
+	bool ShouldPlatformReturn() const;
+	float GetDistanceMoved() const;
 };
